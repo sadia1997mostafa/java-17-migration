d8edfd7747b51ad5505ff951d163a6562ce70cbc
sadia1997mostafa <mostafa2107095@stud.kuet.ac.bd>
2025-09-21 06:55:29 +0600

    daaq

---
+++ ini.java
@@ -0,0 +1,35 @@
+ {
+
+   "on": {
+     "push": {
+       "branches": ["**"]
+     },
+     "pull_request": {
+       "branches": ["main"]
+     }
+   },
+   "jobs": {
+     "build": {
+       "runs-on": "ubuntu-latest",
+       "steps": [
+         {
+           "name": "Checkout repository",
+           "uses": "actions/checkout@v4"
+         },
+         {
+           "name": "Set up JDK",
+           "uses": "actions/setup-java@v4",
+           "with": {
+             "distribution": "temurin",
+             "java-version": "17"
+           }
+         },
+         {
+           "name": "Compile Java",
+           "run": "javac *.java"
+         }
+       ]
+     }
+   }
+}
+// dfhjrf,ehggejyerhy
